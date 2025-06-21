f20c7dfa6352d5ab333fd7d2eacfd54dad17e08e
physwizz <pollockg86@gmail.com>
2025-06-21 14:34:27 +1000

    setup toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,9 +5,12 @@ OUT_DIR=out
 
 # you should change the "CROSS_COMPILE" to right toolchain path (you downloaded)
 # ex)CROSS_COMPILE={android platform directory you downloaded}/android/prebuilt/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=arm-linux-androideabi- KCFLAGS=-mno-android"
+# export CROSS_COMPILE=/home/physwizz/toolchains/arm-linux-androideabi-5.1-master/bin/arm-linux-androideabi-
 
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
+
+COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=/toolchain/arm-linux-androideabi-5.1-master/bin/arm-linux-androideabi- KCFLAGS=-mno-android"
+
+export PATH=$(pwd)/toolchain/arm-linux-androideabi-5.1-master/bin:$PATH
 export ARCH=arm
 
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
